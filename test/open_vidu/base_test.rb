ab84b6e2b4bb88a07dd5b62a757fdcb01771c168
Bryan Saxon <emailBryanSaxon@gmail.com>
2019-09-30 13:13:42 -0500

    add name comments to test files

--- test/open_vidu/base_test.rb
+++ test/open_vidu/base_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 module OpenVidu
+  # BaseTest
   class BaseTest < Minitest::Test
     def test_that_it_works
       assert true
--- test/open_vidu_test.rb
+++ test/open_vidu_test.rb
@@ -1,5 +1,6 @@
 require 'test_helper'
 
+# OpenViduTest
 class OpenViduTest < Minitest::Test
   def test_it_has_a_version_number
     refute_nil ::OpenVidu::VERSION
